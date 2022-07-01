13166cbfda50b322a248558868baa2f92a2b8c0e
Melisa <eu_melisa_andra@yahoo.com>
2022-07-01 09:41:39 +0300

    Remake golfer page

--- pages/golfers/[id].js
+++ pages/golfers/[id].js
@@ -1,5 +1,4 @@
 import { useRouter } from 'next/router'
-import useUser from '../../lib/useUser'
 import useUserScores from '../../lib/useUserScores'
 import Layout from '../../components/Layout'
 import ScoreCard from '../../components/ScoreCard'
@@ -7,44 +6,27 @@ import ScoreCard from '../../components/ScoreCard'
 const GolferPage = () => {
   const router = useRouter()
   const { id } = router.query
-
-  const { user, error: userError } = useUser(id)
-  const { scores, error: scoresError } = useUserScores(id)
+  const { scores, error } = useUserScores(id)
 
   return (
     <>
       <Layout>
-        { userError ? (
-          userError
+        { error ? (
+          error
         ) : (
           <>
-            { user && (
-              <div className="text-2xl">
-                <h1>{user.name}</h1>
-              </div>
-            )}
+            {scores && scores.map(score => (
+              <ScoreCard
+                key={score.id}
+                id={score.id}
+                totalScore={score.total_score}
+                playedAt={score.played_at}
+                userId={score.id}
+                userName={score.user_name}
+              />
+            ))}
           </>
-        )}
-        <ul>
-          <>
-            {scoresError ? (
-              scoresError
-            ) : (
-              <>
-                {scores && scores.map(score => (
-                  <ScoreCard
-                    key={score.id}
-                    id={score.id}
-                    totalScore={score.total_score}
-                    playedAt={score.played_at}
-                    userId={user.id}
-                    userName={user.name}
-                  />
-                ))}
-              </>
-            )}
-          </>
-        </ul>
+          )}
       </Layout>
     </>
   )
